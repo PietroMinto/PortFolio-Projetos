6a600531d86c230bdec0e4fbdd46e5c3463e1fc3
PietroMinto <pietrofulanminto@gmail.com>
2024-10-10 22:23:55 -0300

    Primeiro commit do Meu PortFolio

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,69 @@
+import facebook from "../../assets/facebook.png";
+import instagram from "../../assets/instagram.png";
+import linkedin from "../../assets/linkedin.png";
+import Gmail from "../../assets/gmail.png";
+import MyProfilePhoto from "../../assets/eu.jpg";
+
+import {
+  Button,
+  Container,
+  Header,
+  LeftContainer,
+  Links,
+  RightContainer,
+  SocialMedia,
+  SocialProfile,
+  Title,
+} from "./styles";
+
+export function Home() {
+  return (
+    <Container>
+      <Header>
+        <h1>
+          Bem vindo(a) ao meu <span>PortFolio</span>
+        </h1>
+        <Links>Home</Links>
+        <Links>Projetos</Links>
+        <Links>Sobre Mim</Links>
+        <Links>Contato</Links>
+      </Header>
+      <LeftContainer>
+        <Title>
+          <h2>
+            Olá, meu nome é <span>Pietro</span> sou um Dev FullStack
+          </h2>
+        </Title>
+        <p>
+          Tenho habilidades para fazer, sites landing pages, aplicações de
+          cadastro e login de usuário, e um pouco da parte do Back-End e também
+          na construção de alguns servidores, já produzi algumas aplicações
+          responsivas também. Atualmente sou melhor no Front-End, gosto sempre
+          de ficar criando projetos diferetes para praticar e melhorar cada vez
+          mais minhas técnicas e habilidades nessa área que tanto gosto.
+        </p>
+        <SocialProfile>
+          <Button href="https://w.app/19zBWZ">Contato pessoal</Button>
+          <Button>Meu GitHub</Button>
+          <SocialMedia>
+              <a href="https://pt-br.facebook.com/login/device-based/regular/login/">
+                <img src={facebook} />
+              </a>
+              <a href="https://www.instagram.com/pietro_f_minto/">
+                <img src={instagram} />
+              </a>
+              <a href="https://www.linkedin.com/in/pietro-minto/">
+                <img src={linkedin} />
+              </a>
+              <a href="https://w.app/19zBWZ">
+                <img src={Gmail} />
+              </a>
+          </SocialMedia>
+        </SocialProfile>
+      </LeftContainer>
+      <RightContainer>
+        <img src={MyProfilePhoto} />
+      </RightContainer>
+    </Container>
+  );
+}
---
+++ src/Pages/Home/styles.js
@@ -0,0 +1,144 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #333;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+`;
+
+export const LeftContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+
+  height: 100%;
+  width: 100%;
+  max-width: 50%;
+  max-height: 90%;
+  margin-left: 50px;
+
+  span {
+    color: #ffd700;
+  }
+
+  p {
+    font-size: 20px;
+    color: #fff;
+  }
+
+  &:hover a:hover img {
+    transform: translateY(-10px);
+    transition: ease-in-out 0.3s;
+  }
+
+  &:active a:active img {
+    opacity: 0.8;
+    transition: none;
+  }
+`;
+
+export const RightContainer = styled.div`
+  position: absolute;
+  right: 200px;
+  top: 150px;
+
+
+  width: 400px;
+  height: 400px;
+  border-radius: 50%;
+  border: 5px solid #f1c40f;
+  padding: 5px;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 50%;
+  }
+
+  &:hover {
+    scale: 1.1;
+    transition: ease-in-out 0.5s;
+  }
+`;
+
+export const Header = styled.div`
+  position: absolute;
+  background-color: #666;
+  height: 10%;
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  gap: 20px;
+  padding: 20px;
+
+  h1 {
+    position: absolute;
+    left: 15px;
+    top: 20px;
+
+    font-size: 40px;
+    font-family: "Protest Strike", sans-serif;
+    color: #dcdcdc;
+  }
+
+  span {
+    color: #ffd700;
+    font-family: "Protest Strike", sans-serif;
+  }
+`;
+
+export const Links = styled.a`
+  font-size: 24px;
+  font-family: "Protest Strike", sans-serif;
+  color: #dcdcdc;
+  cursor: pointer;
+
+  &:hover {
+    color: #ffd700;
+    transition: ease-in-out 0.3s
+  }
+`;
+
+export const Title = styled.h2`
+  font-size: 32px;
+  color: #dcdcdc;
+`;
+
+export const SocialProfile = styled.div``;
+
+export const SocialMedia = styled.div`
+  display: flex;
+  margin-left: 20px;
+  gap: 20px;
+  margin-top: 20px;
+  cursor: pointer;
+
+  img {
+    max-width: 80px;
+  }
+`;
+
+export const Button = styled.button`
+  background-color: transparent;
+  height: 52px;
+  width: 200px;
+  margin: 20px;
+
+  gap: 50px;
+  border: 2px inset #ffd700;
+  border-radius: 5px;
+  font-size: 20px;
+  cursor: pointer;
+
+  &:hover {
+    scale: 1.1;
+    transition: ease-in-out 0.3s;
+  }
+`;
